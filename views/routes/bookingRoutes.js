8e2e6ec85578c49b4a2c01ce935e02d74c4502c8
Moez <57503360+moezbenrebah@users.noreply.github.com>
2022-02-07 09:13:53 +0100

    pushed by error

--- views/routes/bookingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const bookingController = require('../controllers/bookingController');
-const authController = require('../controllers/authController');
-
-const router = express.Router();
-
-router.get('/checkout-stripe/:travelId', authController.grantAcess, bookingController.getCheckoutStripe);
-
-module.exports = router;
