882b518a507f4660f6c4b5ca568fbc18507f9eaf
moezbenrebah <benrebah.moez@gmail.com>
2022-02-15 15:11:50 +0100

    improve CORS

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
@@ -82,7 +81,11 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 // use the stripe webhook in order to create a new booking travel
-app.post('/webhook-checkout', bodyParser.raw({ type: '*/*' }), bookingController.webhookCheckout)
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application / json' }),
+  bookingController.webhookCheckout
+);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
