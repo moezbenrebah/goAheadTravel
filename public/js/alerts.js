ca038fe286a1aa69e1620ea6dd4f7921b9c419df
Moez <57503360+moezbenrebah@users.noreply.github.com>
2022-02-18 21:36:53 +0100

    Update alerts.js

--- public/js/alerts.js
+++ public/js/alerts.js
@@ -1,12 +1,12 @@
 /* eslint-disable */
 export const hideAlert = () => {
-	const element = document.querySelector('.alert');
-	if (element) element.parentElement.removeChild(element);
+  const element = document.querySelector('.alert');
+  if (element) element.parentElement.removeChild(element);
 }
 
 export const showAlert = (type, msg) => {
-	hideAlert();
+  hideAlert();
   const markup = `<div class="alert alert--${type}">${msg}</div>`;
-	document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
-	window.setTimeout(hideAlert, 5000);
+  document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+  window.setTimeout(hideAlert, 5000);
 }
