03824393d28c8d4bdde79acab867ba4a83ce3b61
Moez <57503360+moezbenrebah@users.noreply.github.com>
2022-02-18 17:56:45 +0100

    fix indentation

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -4,61 +4,69 @@ const Booking = require('../models/bookingModel');
 const catchAsyncHandler = require('../utilities/catchAsyncHandler');
 const ErrHandlingClass = require('../utilities/errorHandlingClass');
 
+//** get the the overview page
 exports.getTravelOverview = catchAsyncHandler(async (req, res, next) => {
-	const travels = await Travel.find();
-	res.status(200).render('overview', {
-		title: ' | All Travels',
-		travels,
-	});
+  const travels = await Travel.find();
+  res.status(200).render('overview', {
+    title: 'All Travels',
+    travels,
+  });
 });
 
+//** get travel details page
 exports.getTravelDetail = catchAsyncHandler(async (req, res, next) => {
-	const travel = await Travel.findOne({slug: req.params.slug}).populate({
-		path: 'ratings',
-		fields: 'review rating user'
-	});
+  const travel = await Travel.findOne({slug: req.params.slug}).populate({
+    path: 'ratings',
+    fields: 'review rating user'
+  });
 
-	if (!travel) {
-		return next(new ErrHandlingClass('There is no travel with such name', 404))
-	}
+  if (!travel) {
+    return next(new ErrHandlingClass('There is no travel with such name', 404))
+  }
 
-	res.status(200).render('travel', {
-		title: `${travel.name} Travel`,
-		travel
-	});
+  res.status(200).render('travel', {
+    title: `${travel.name} Travel`,
+    travel
+  });
 });
 
+//** get the login page
 exports.accountLogin = (req, res) => {
-	res.status(200).render('login', {
-		title: 'log in please'
-	});
+  res.status(200).render('login', {
+    title: 'log in please'
+  });
 };
 
+//** get the signup page
 exports.accountSignup = (req, res) => {
-	res.status(200).render('signup', {
-		title: 'signup please'
-	});
+  res.status(200).render('signup', {
+    title: 'signup please'
+  });
 };
 
+//** get the user account page
 exports.myAccount = (req, res) => {
-	res.status(200).render('profile', {
-		title: 'take a look at your account'
-	});
+  res.status(200).render('profile', {
+    title: 'take a look at your account'
+  });
 };
 
+//** get the forgot password page
 exports.forgotPassword = (req, res) => {
-	res.status(200).render('forgotPassword', {
-		title: 'forgotpassword'
-	});
+  res.status(200).render('forgotPassword', {
+    title: 'forgotpassword'
+  });
 };
 
+//** get the reset password page
 exports.resetPassword = (req, res) => {
   res.status(200).render('resetPassword', {
-		title: 'reset password',
-		token: req.params.token
-	});
+    title: 'reset password',
+    token: req.params.token
+  });
 };
 
+//** handle users' data updating
 exports.updateUserData = catchAsyncHandler(async (req, res, next) => {
   const updatedUser = await User.findByIdAndUpdate(
     req.user.id,
@@ -78,18 +86,18 @@ exports.updateUserData = catchAsyncHandler(async (req, res, next) => {
   });
 });
 
-// get all booked travels per user
+//** get all booked travels per user
 exports.myBookedTravels = catchAsyncHandler( async(req, res, next) => {
-	// find all booked travels based on user id
-	const bookedTravels = await Booking.find({ user: req.user.id });
+  // find all booked travels based on user id
+  const bookedTravels = await Booking.find({ user: req.user.id });
 
-	// find all travels that includes the above id (bookedTravels)
-	const travelsIds = bookedTravels.map(item => item.travel);
-	const travels = await Travel.find({ _id: { $in: travelsIds } });
+  // find all travels that includes the above id (bookedTravels)
+  const travelsIds = bookedTravels.map(item => item.travel);
+  const travels = await Travel.find({ _id: { $in: travelsIds } });
 
-	// Render the page that contains the booked travels
-	res.status(200).render('overview', {
-		title: 'My travels',
-		travels
-	})
-})
+  // Render the page that contains the booked travels
+  res.status(200).render('overview', {
+    title: 'My travels',
+    travels
+  });
+});
